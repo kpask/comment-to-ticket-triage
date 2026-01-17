3c61a9f01ad125551c2b52558469d68a52309fb0
Karolis Paskevicius <kp704595@gmail.com>
2026-01-17 16:26:53 +0200

    Add H2 config, update Comment and Ticket from records to JPA entities, add relationship mapping

--- src/main/java/com/example/pulsedesk/models/Comment.java
+++ src/main/java/com/example/pulsedesk/models/Comment.java
@@ -1,9 +1,45 @@
 package com.example.pulsedesk.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-public record Comment(
-    int id,
-    String text,
-    LocalDateTime createdAt
-){}
+@Entity
+public class Comment{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String text;
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    public Comment(){}
+
+    public Comment(String text){
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+}
--- src/main/java/com/example/pulsedesk/models/Ticket.java
+++ src/main/java/com/example/pulsedesk/models/Ticket.java
@@ -2,12 +2,87 @@ package com.example.pulsedesk.models;
 
 import com.example.pulsedesk.enums.Category;
 import com.example.pulsedesk.enums.Priority;
+import jakarta.persistence.*;
 
-public record Ticket(
-    int id,
-    Category category,
-    Priority priority,
-    String title,
-    String summary,
-    Comment comment
-){}
+import java.time.LocalDateTime;
+
+
+@Entity
+public class Ticket{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String title;
+    private String summary;
+
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    @Enumerated(EnumType.STRING)
+    private Priority priority;
+
+    @OneToOne
+    @JoinColumn(name = "comment_id")
+    private Comment comment;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    public Ticket(){}
+
+    public Ticket(String title, String summary, Category category, Priority priority, Comment comment){
+        this.category = category;
+        this.priority = priority;
+        this.title = title;
+        this.summary = summary;
+        this.comment = comment;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public Priority getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Priority priority) {
+        this.priority = priority;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public Comment getComment() {
+        return comment;
+    }
+
+    public void setComment(Comment comment) {
+        this.comment = comment;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
